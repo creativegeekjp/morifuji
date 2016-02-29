f9be370f86273f04db65d541fa7d516d5f48fa95
creativegeek <info@creativegeek.jp>
2016-02-29 09:54:04 +0000

    Slider on top Page

--- wp-content/themes/morifuji/header.php
+++ wp-content/themes/morifuji/header.php
@@ -16,6 +16,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo(''); ?>" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
